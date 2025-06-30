0c4cdf27e383e942c486eb73919fdd447d13a138
Denys Omelianenko <denome@auditdata.com>
2025-06-30 15:16:46 +0300

    fixed menu, moved menu data to the separete json file

---
+++ Irisha/Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Irisha.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Href { get; set; } = string.Empty;
+    }
+}
